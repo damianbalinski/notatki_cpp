74d68c8d90783a7b2da4aa2884264f9f4d9a751a
gumises <gumis5151@gmail.com>
2020-07-19 12:32:51 +0200

    Dodaj pliki projektów.

---
+++ using_declaration.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	using std::cout;
+
+	cout << "Hello world!" << std::endl;
+	cout << "Hello world2!" << std::endl;
+
+	return 0;
+}
---
+++ using_namespace.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "Hello world!" << endl;
+	cout << "Hello world2!" << endl;
+
+	return 0;
+}
