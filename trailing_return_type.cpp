9b40e6faa66c875e3c7e73e737a0bbb02a08a6ab
gumises <gumis5151@gmail.com>
2020-07-29 19:02:05 +0200

    deklaracja opozniona typu zwracanego

--- trailing_return_type.cpp
+++ trailing_return_type.cpp
@@ -1,22 +1,48 @@
+// OPOZNIONA DEKLARACJA TYPU ZWRACANEGO
 #include <iostream>
 
 /*
- * OPOZNIONA DEKLARACJA TYPU ZWRACANEGO
+ * Deklaracja jawna.
  */
-int func1(int a, float b)
+double func1(int a, int b)
 {
 	return a + b;
 }
 
+/*
+ * Dedukcja automatyczna
+ * Nie dziala dla referencji.
+ */
 auto func2(int a, float b)
 {
+	// return a + b;
+	return;
+}
+
+/*
+ * Deklaracja opozniona.
+ */
+auto func3(int a, int b) -> double
+{
 	return a + b;
 }
 
-auto func3(int a, float b)
+/*
+ * Deklaracja opozniona z szablonem.
+ */
+template<typename T, typename U>
+auto func4(T t, U u) -> decltype(t + u)
 {
-	decltype(a + b) c = a + b;
-	return c;
+	return t + u;
+}
+
+/*
+ * Deklaracja opozniona z szablonem.
+ */
+template<typename T, typename U>
+decltype(std::declval<T>() + std::declval<U>()) func5(T t, U u)
+{
+	return t + u;
 }
 
 // ERROR: a+b nie sa w zasiegu parametrow funkcji
@@ -25,11 +51,6 @@ auto func3(int a, float b)
 //	return a + b;
 //}
 
-auto func5(int a, float b) -> decltype(a + b)
-{
-	return a + b;
-}
-
 int main()
 {
 	using namespace std;
@@ -37,13 +58,13 @@ int main()
 	auto a1 = func1(1, 1.);
 	auto a2 = func2(1, 1.);
 	auto a3 = func3(1, 1.);
-	//auto a4 = func4(1, 1.);
+	auto a4 = func4(1, 1.);
 	auto a5 = func5(1, 1.);
 
 	cout << typeid(a1).name() << endl;
 	cout << typeid(a2).name() << endl;
 	cout << typeid(a3).name() << endl;
-	//cout << typeid(a4).name() << endl;
+	cout << typeid(a4).name() << endl;
 	cout << typeid(a5).name() << endl;
 
 	return 0;
