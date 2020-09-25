bff135829efa3735f6f559fc73704a3afbd5050a
gumises <gumis5151@gmail.com>
2020-09-25 11:33:54 +0200

    literaly
    - literaly lancuchowe
    - literaly wskaznikowe

---
+++ pointer_literal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+ * LITERAL WSKAZNIKOWY
+ */
+
+using namespace std;
+
+int main()
+{
+	nullptr;
+
+	return 0;
+}
--- string_literals.cpp
+++ string_literals.cpp
@@ -10,23 +10,16 @@ using namespace std;
 
 int main()
 {
-	// ZWYKLE	// SUROWE
+	// NORMALNY	// SUROWY
 	"abc";		R"(abc)";		// char*
 	u8"abc";	u8R"(abc)";		// char8_t*		UTF-8
-	u"abc";		UR"(abc)";		// char16_t*	UTF-16
+	u"abc";		uR"(abc)";		// char16_t*	UTF-16
 	U"abc";		UR"(abc)";		// char32_t*	UTF-32
 	L"abc";		LR"(abc)";		// wchar_t*
 
 	// RAW STRING DELIMITER
 	R"(---)";		// bez delimitera
 	R"abc(---)abc";	// z delimiterem
-
-	// TEST
-	cout << sizeof("abc") << endl;
-	cout << sizeof(u8"abc") << endl;
-	cout << sizeof(u"abc") << endl;
-	cout << sizeof(U"abc") << endl;
-	cout << sizeof(L"abc") << endl;
 
 	return 0;
 }
