7347faf315fe99ea937ac9041e1f70b7e55e6f8d
gumises <gumis5151@gmail.com>
2020-09-26 09:06:46 +0200

    lemsymy alternatywne

--- alternative_operator_names.cpp
+++ alternative_operator_names.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 
+/*
+ * LEKSEMY ALTERNATYWNE
+ * alternatywne nazwy dla operatorow
+ */
+
 int main()
 {
-	// OPERATORY LOGICZNE
-	1 and 0;	// 1 && 1;
-	1 or 0;		// 1 || 0;
-	not 1;		// !1;
+	// DANE
+	bool b1 = true;
+	bool b2 = false;
+
+	// LOGICZNE
+	1 and 0;			1 && 1;
+	1 or 0;				1 || 0;
+	not 1;				!1;
+
+	// BITOWE
+	1 bitand 0;			1 & 0;
+	1 bitor 0;			1 | 0;
+	1 xor 0;			1 ^ 0;
+	compl 1;			~1;
+
+	// PRZYPISANIE BITOWE
+	b1 and_eq b2;		b1 &= b2;
+	b1 xor_eq b2;		b1 ^= b2;
+	b1 or_eq b2;		b1 |= b2;
+	b1 not_eq b2;		b1 != b2;
 
 	return 0;
 }
---
+++ literal_operator_template.cpp
@@ -0,0 +1,1 @@
+//TODO literal operator template
